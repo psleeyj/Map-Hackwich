72f45779bd843659f0eebb174ac3669752b8eb9d
Kate Lee <isoeun809@gmail.com>
2022-07-27 09:45:41 -0500

    Initial Commit

---
+++ Map Hackwich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Map Hackwich
+//
+//  Created by 이소은 on 2022/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Map Hackwich/Map_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwichApp.swift
+//  Map Hackwich
+//
+//  Created by 이소은 on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
