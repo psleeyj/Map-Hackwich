91784f37f1b5994f961698d3872a7ddf89147cfd
Kate Lee <isoeun809@gmail.com>
2022-07-27 10:26:44 -0500

    Added a location to the map

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -20,17 +20,47 @@ struct ContentView: View {
     )
     @StateObject var locationManager = LocationManager()
     @State private var userTrackingMode: MapUserTrackingMode = .follow
+    @State private var places = [Place(name: "Northwestern University", coordinate: CLLocationCoordinate2D(latitude: 42.0558, longitude: -87.6743))]
     
     var body: some View {
         Map(coordinateRegion: $region,
             interactionModes: .all,
             showsUserLocation: true,
-            userTrackingMode: $userTrackingMode)
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places){
+            place in MapAnnotation(coordinate: place.coordinate, anchorPoint: CGPoint(x: 0.5, y: 1.2)){
+                Marker(name: place.name)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Place: Identifiable{
+    let id = UUID()
+    let name: String
+    let coordinate : CLLocationCoordinate2D
+}
+
+struct Marker: View{
+    var name: String
+    var body: some View{
+        ZStack{
+            VStack{
+                Spacer(minLength: 15)
+                Rectangle()
+                    .fill(.black)
+                    .frame(width: 30, height: 30, alignment: .center)
+            }
+            Capsule()
+                .fill(.red)
+                .frame(width: 200, height: 30, alignment: .center)
+            Text(name)
+        }
     }
 }
